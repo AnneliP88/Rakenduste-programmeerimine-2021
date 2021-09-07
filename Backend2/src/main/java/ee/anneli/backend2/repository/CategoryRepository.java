1eebf74926042c6f633e65b623495d9ccb5a6af8
Anneli Põldaru <55189772+AnneliP88@users.noreply.github.com>
2021-09-07 19:39:25 +0300

    1. homework first attempt

---
+++ Backend2/src/main/java/ee/anneli/backend2/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package ee.anneli.backend2.repository;
+
+import ee.anneli.backend2.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
