46a5b760d251657e1b0c084c6be208cda47b6861
Anneli Põldaru <55189772+AnneliP88@users.noreply.github.com>
2021-10-14 18:02:17 +0300

    3rd homework. Update Item (with JSON body). Increment item's quality by 1. And updatedAt gets updated.

--- Course files (lecturer Raimo on Fridays)/backend-node/src/controllers/item.js
+++ Course files (lecturer Raimo on Fridays)/backend-node/src/controllers/item.js
@@ -7,22 +7,50 @@ exports.getItems = async (req, res) => {
 }
 
 exports.createItem = async (req, res) => {
-  const newItem = {
-    name: "Table",
-    quality: 99,
-    unused: true,
-    color: "blue"
-  }
-
-  const createdItem = new Item(newItem)
+  // Commented it out, because I wanted to give new data through body (JSON object)
+  // const newItem = {
+  //   name: "Table",
+  //   quality: 99,
+  //   unused: true,
+  //   color: "blue"
+  // }
+  // const createdItem = new Item(newItem)
+  const createdItem = new Item(req.body)
 
   const savedItem = await createdItem.save()
 
   res.status(200).send(`yay ${savedItem._id}`)
 }
 
+// Homework 3
+//  https://mongoosejs.com/docs/tutorials/findoneandupdate.html
+//  https://mongoosejs.com/docs/api.html#model_Model.findOneAndUpdate
 exports.updateItem = async (req, res) => {
+  const { id } = req.params;
+  const queryFilter = { _id: id};
+  const update = req.body;
+  const returnModifiedItem = { new: true };
 
+  const updatedItem = await Item.findOneAndUpdate(queryFilter, update, returnModifiedItem)
+
+  if (!updatedItem) res.status(404).send(`Item with id:${id} was not found`)
+  res.status(200).send(`Successfully found and updated the following item: \n ${updatedItem}`)
+}
+
+// Homework 3
+// Only Item's quality (during the query) and updatedAt (automatically) fields are updated.
+// https://docs.mongodb.com/manual/reference/method/db.collection.findOneAndUpdate/#examples
+exports.incrementItemQuality = async (req, res) => {
+  const { id } = req.params;
+
+  const updatedItem = await Item.findOneAndUpdate(
+    { _id: id}, 
+    { $inc: { quality: 1 }}, 
+    { new: true }
+  )
+
+  if (!updatedItem) res.status(404).send(`Item with id:${id} was not found`)
+  res.status(200).send(`Successfully updated the following item's quality: \n ${updatedItem}`)
 }
 
 exports.deleteItem = async (req, res) => {
@@ -33,4 +61,12 @@ exports.deleteItem = async (req, res) => {
   if (!item) res.status(404).send("No item with that id found")
 
   res.status(200).send(`Successfully deleted the following item: \n ${item}`)
+}
+
+exports.deleteAllItems = async (req, res) => {
+  const deletedItems = await Item.deleteMany()
+
+  if (!deletedItems) res.status(404).send(`There are no Items in DB`)
+
+  res.status(200).send(`Successfully deleted ${deletedItems.deletedCount} item(s).\n`)
 }
--- Course files (lecturer Raimo on Fridays)/backend-node/src/models/Item.js
+++ Course files (lecturer Raimo on Fridays)/backend-node/src/models/Item.js
@@ -1,12 +1,17 @@
 const { Schema, model } = require('mongoose')
 
+// Homework:
+// Võib ka alternatiivina lisada skeemasse updatedAt ja uuendada seda, kui uuendus toimub
+// Timestamp adds createdAt and updatedAt fields
+// https://masteringjs.io/tutorials/mongoose/timestamps
 const itemSchema = new Schema({
   name: { type: String, required: true },
   quality: { type: Number, required: true },
   unused: { type: Boolean, default: true },
-  color: { type: String, enum: ['red', 'green', 'blue'], default: 'green' },
-  createdAt: { type: Date, default: Date.now }
-});
+  color: { type: String, enum: ['red', 'green', 'blue'], default: 'green' }
+},
+{ timestamps: true }
+);
 
 const Item = model("Item", itemSchema)
 
